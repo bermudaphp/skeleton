a223394589373871b4cadd1e7c72344edfd762e4
Andrey <alestro@bk.ru>
2020-07-16 12:21:33 +0200

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+declare(strict_types=1);
+
+
+error_reporting(E_ALL);
+
+if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
+    return false;
+}
+
+define('APP_ROOT', dirname(__DIR__));
+
+chdir(APP_ROOT);
+
+require APP_ROOT . '/vendor/autoload.php';
+
+(function()
+{
+    $app = (new \Bermuda\App\AppFactory())(require APP_ROOT . '/config/container.php');
+
+    require APP_ROOT . '/config/routes.php';
+    require APP_ROOT . '/config/pipeline.php';
+
+    $app->run();
+})();
