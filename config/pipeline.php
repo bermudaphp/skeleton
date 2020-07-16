2c54774160f574dad6d3bddfeee7fe53e665f3b0
Andrey <alestro@bk.ru>
2020-07-16 17:17:19 +0200

    Update pipeline.php

--- config/pipeline.php
+++ config/pipeline.php
@@ -1,25 +1,11 @@
 <?php
 
+
 /**
  * @var \Bermuda\App\AppInterface $app
  */
 
-    /*
-    $app->pipe(static function (ServerRequestInterface $request, RequestHandlerInterface $handler) use ($app)
-    {
-        $bootstrapper = $app->get(Bootstrapper::class);
 
-        $bootstrapper->add(MyBootable::class);
-
-        return $handler->handle($request);
-    });
-    */
-
-$app->pipe(Bermuda\ErrorHandler\ErrorHandlerMiddleware::class);
-//$app->pipe(Authenticator::class);
-
-/**
- * Routing middleware
- */
+$app->pipe(\Bermuda\ErrorHandler\ErrorHandlerMiddleware::class);
 $app->pipe(\Bermuda\Router\Middleware\MatchRouteMiddleware::class);
 $app->pipe(\Bermuda\Router\Middleware\DispatchRouteMiddleware::class);
