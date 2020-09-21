adc2607565f190669924f38b0731e3a3de1dea46
Andrey <inbox.shelamkoff@gmail.com>
2020-09-21 22:26:58 +0300

    Update ConfigProvider.php

--- app/ConfigProvider.php
+++ app/ConfigProvider.php
@@ -10,17 +10,60 @@ namespace App;
  */
 class ConfigProvider
 {
+    public const dependecies = 'dependencies';
+    public const factories = 'factories';
+    public const invokables = 'invokables';
+    public const autowires = 'autowires';
+    public const aliases 'aliases';
+    public const delegators = 'delegators';
+    public const services = 'services';
+    
     public function __invoke(): array
     {
         return [
-            'dependencies' => $this->getDependencies()
+            self::dependencies => $this->getDependencies()
         ];
     }
     
-    private function getDependencies(): array
+    protected function getDependencies(): array
     {
         return [
-            'factories' => []
+            self::factories => $this->getFactories(),
+            self::invokables => $this->getInvokables(),
+            self::autowires => $this->getAutowires(),
+            self::aliases => $this->getAliases(),
+            self::delegators => $this->getDelegators(),
+            self::services => $this->getServices(),
         ];
+    }
+    
+    protected functions getFactories(): array
+    {
+        return [];
+    }
+    
+    protected functions getInvokables(): array
+    {
+        return [];
+    }
+    
+    protected functions getAutowires(): array
+    {
+        return [];
+    }
+    
+    protected functions getAliases(): array
+    {
+        return [];
+    }
+    
+    protected functions getDelegators(): array
+    {
+        return [];
+    }
+    
+    protected functions getServices(): array
+    {
+        return [];
     }
 }
