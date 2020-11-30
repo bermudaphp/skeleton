c467a09e3cf4fd139f69952285e01cbd9b0b3edb
Andrey <inbox.shelamkoff@gmail.com>
2020-11-30 22:29:43 +0300

    Create DisplaysAppVersionCommand.php

---
+++ bin/commands/DisplaysAppVersionCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Bermuda\App\AppInterface;
+use Bermuda\App\Console\CommandInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+/**
+ * Class VersionCommand
+ * @package App\Console\Commands
+ */
+final class DisplaysAppVersionCommand implements CommandInterface
+{
+    private AppInterface $app;
+
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
+
+    public function __invoke(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln($this->app->version());
+        return self::success;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'version';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return 'Displays app current version';
+    }
+}
