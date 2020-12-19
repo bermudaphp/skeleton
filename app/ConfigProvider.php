4c83ad75b84e162fd7e5236b7da931b2cf786f6b
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2020-12-19 19:28:58 +0300

    Update ConfigProvider.php

--- app/ConfigProvider.php
+++ app/ConfigProvider.php
@@ -1,43 +1,23 @@
 <?php
 
-
 namespace App;
+
+
+use Bermuda\App\AppFactory;
+use Bermuda\App\AppInterface;
+use Bermuda\App\Boot\Bootstrapper;
+use Bermuda\App\Boot\RouteBootstrapper;
+use Bermuda\App\Boot\RouterBootstrapper;
+use Bermuda\App\Boot\BootstrapperInterface;
+use Bermuda\Provider\ConfigProvider as BermudaConfigProvider;
 
 
 /**
  * Class ConfigProvider
  * @package App
  */
-class ConfigProvider
-{
-    public const dependencies = 'dependencies';
-    public const factories = 'factories';
-    public const invokables = 'invokables';
-    public const autowires = 'autowires';
-    public const aliases = 'aliases';
-    public const delegators = 'delegators';
-    public const services = 'services';
-    
-    public function __invoke(): array
-    {
-        return [
-            'app' => ['version' => '1.0'],
-            self::dependencies => $this->getDependencies(),
-        ];
-    }
-    
-    protected function getDependencies(): array
-    {
-        return [
-            self::factories => $this->getFactories(),
-            self::invokables => $this->getInvokables(),
-            self::autowires => $this->getAutowires(),
-            self::aliases => $this->getAliases(),
-            self::delegators => $this->getDelegators(),
-            self::services => $this->getServices(),
-        ];
-    }
-    
+final class ConfigProvider extends BermudaConfigProvider
+{  
     /**
      * An associative array that maps a service name to a factory class name, or any callable. 
      * Factory classes must be instantiable without arguments, and callable once instantiated (i.e., implement the __invoke() method).
@@ -45,55 +25,15 @@ class ConfigProvider
      */
     protected function getFactories(): array
     {
-        return [];
-    }
-    
-    /**
-     * An associative array that map a key to a constructor-less service; i.e., for services that do not require arguments to the constructor. 
-     * The key and service name usually are the same; 
-     * if they are not, the key is treated as an alias. It could also be an array of services.
-     * @return array
-     */
-    protected function getInvokables(): array
-    {
-        return [];
-    }
-    
-    /**
-     * an array of service with or without a constructor; 
-     * PHP-DI offers an autowire technique that will scan the code and see what are the parameters needed in the constructors. 
-     * Any aliases needed should be created in the aliases configuration
-     * @return array
-     */
-    protected function getAutowires(): array
-    {
-        return [];
-    }
-    
-    /**
-     * An associative array that maps an alias to a service name (or another alias).
-     * @return array
-     */
-    protected function getAliases(): array
-    {
-        return [];
-    }
-    
-    /**
-     * An associative array that maps service names to lists of delegator factory keys
-     * @return array
-     */
-    protected function getDelegators(): array
-    {
-        return [];
-    }
-    
-    /**
-     * An associative array that maps a key to a specific service instance or service name.
-     * @return array
-     */
-    protected function getServices(): array
-    {
-        return [];
+        return [
+            AppInterface::class => AppFactory::class,
+            BootstrapperInterface::class => static function(ContainerInterface $container): Bootstrapper
+            {
+                return Bootstrapper::makeOf([
+                    new RouterBootstrapper,
+                    new PipelineBootstrapper,
+                ]);
+            },
+        ];
     }
 }
