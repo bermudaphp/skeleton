b170efa51108f8c028e4520fa10e2b04553b7cd0
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2020-12-20 20:37:18 +0300

    Create Command.php

---
+++ bin/commands/Command.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Console\Commands;
+
+
+use Bermuda\App\Console\CommandInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+
+
+/**
+ * Class Command
+ * @package Console\Commands
+ */
+abstract class Command extends SymfonyCommand implements CommandInterface
+{
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return parent::getName();
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return parent::getDescription();
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     * @throws \Exception
+     */
+    public function __invoke(InputInterface $input, OutputInterface $output): int
+    {
+        return $this->run($input, $output);
+    }
+}
