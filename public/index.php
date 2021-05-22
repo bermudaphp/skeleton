2f0479f71713d3c17c457a1600c30881a9ea5442
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-22 14:58:16 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -12,6 +12,6 @@ if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__)
 define('APP_ROOT', dirname(__DIR__));
 require APP_ROOT . '\vendor\autoload.php';
 
-(static function(\Psr\Container\ContainerInterface $container) => 
+(static fn(\Psr\Container\ContainerInterface $container) =>
     Bermuda\App\Runner::instantiate($container)->run())
 (require APP_ROOT . '\config\container.php');
