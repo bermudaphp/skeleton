77fc5bf7dc721488b966194d69bdbb9ef0c1d547
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-27 15:33:26 +0300

    Update pipeline.php

--- config/pipeline.php
+++ config/pipeline.php
@@ -3,6 +3,5 @@
 /**
  * @var \Bermuda\App\AppInterface $app
  */
-$app->pipe(\Bermuda\ErrorHandler\ErrorHandlerMiddleware::class);
 $app->pipe(\Bermuda\Router\Middleware\MatchRouteMiddleware::class);
 $app->pipe(\Bermuda\Router\Middleware\DispatchRouteMiddleware::class);
