d3b895f6ee20a16b2c4f430aaa162d658518a7ba
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-28 16:48:08 +0300

    Update CreateHandler.php

--- bin/Commands/CreateHandler.php
+++ bin/Commands/CreateHandler.php
@@ -2,6 +2,13 @@
 
 namespace Console\Commands;
 
+use Bermuda\String\Str;
+use Laminas\Code\Generator\ClassGenerator;
+use Laminas\Code\Generator\FileGenerator;
+use Laminas\Code\Generator\MethodGenerator;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,16 +19,14 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 final class CreateHandler extends Command
 {
-    protected static $defaultName = 'create:handler';
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        list($namespace, $classname) = $this->splitClassname($input->getArgument('classname'));
-        $filename = APP_ROOT . '\app\Handler' . $namespace. '\\' . $classname . '.php';
+        list($namespace, $classname) = Str::classnameSplit(
+            $input->getArgument('classname')
+        );
 
-        $handlerCode = $this->getCode();
-        $handlerCode = str_replace([':namespace', ':classname'], [
-            $namespace, $classname
-        ], $handlerCode);
+        $filename = $this->getFilename($classname, $namespace);
+        $handlerCode = $this->getCode($classname, $namespace);
 
         file_put_contents($filename, $handlerCode);
 
@@ -30,47 +35,51 @@ final class CreateHandler extends Command
         return self::SUCCESS;
     }
 
+    public function getName(): string
+    {
+        return 'create-handler';
+    }
+
     protected function configure()
     {
         $this->addArgument('classname', InputArgument::REQUIRED);
     }
 
-    /**
-     * @param string $classname
-     * @return array
-     */
-    private function splitClassname(string $classname): array
+    private function getCode(string $classname, ?string $namespace = null): string
     {
-        $result = explode('\\', $classname);
-        $classname = array_pop($result);
+        return (new FileGenerator([
+            'classes' => [
+                (new ClassGenerator($classname,
+                    null, null,
+                    null, [RequestHandlerInterface::class],
+                    [], [
+                        new class extends MethodGenerator
+                        {
+                            public function generate()
+                            {
+                                $out = '    /**';
+                                $out .= PHP_EOL . '     * @inheritDoc';
+                                $out .= PHP_EOL . '     */' . PHP_EOL;
+                                $out .= '    public function handle(ServerRequestInterface $request): ResponseInterface';
+                                $out .= PHP_EOL . '    {';
+                                $out .= PHP_EOL . '        // TODO Implement request handling logic';
+                                $out .= PHP_EOL . '    }';
 
-        return ['\\' . implode('\\', $result), $classname];
+                                return $out;
+                            }
+                        }
+                    ]
+                ))->addUse(ServerRequestInterface::class)
+                    ->addUse(ResponseInterface::class)
+                    ->addUse(RequestHandlerInterface::class)
+            ]
+        ]))
+            ->setNamespace($namespace == null ? 'App\Handler' : 'App\Handler\\' . $namespace)
+            ->generate();
     }
 
-    private function getCode(): string
+    private function getFilename(string $classname, ?string $namespace = null): string
     {
-        return "<?php
-
-namespace App\\Handler:namespace;
-
-use Psr\\Http\\Message\\ResponseInterface;
-use Psr\\Http\Message\\ServerRequestInterface;
-use Psr\\Http\\Server\\RequestHandlerInterface;
-
-/**
- * Class :classname
- * @package App\\Handler:namespace
- */
-final class :classname implements RequestHandlerInterface
-{
-    /**
-     * @inheritDoc
-     */
-    public function handle(ServerRequestInterface \$request): ResponseInterface
-    {
-    
-    }
-}";
-
+        return APP_ROOT . '\app\Handler' . $namespace . '\\' . $classname . '.php';
     }
 }
