826ad2856f550a6ad5dd8ced92051657641adb5d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-06-09 20:51:21 +0300

    Create .phpstorm.meta.php

---
+++ .phpstorm.meta.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PHPSTORM_META {
+
+    override(\Psr\Container\ContainerInterface::get(0), map([
+        '' => '@',
+    ]));
+
+    override(\Bermuda\app(0), map([
+        '' => '@',
+    ]));
+}
