c474bd54179325fa3a26a72e86ba072b22b2b471
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-06-09 20:16:41 +0300

    Create Installer.php

---
+++ Installer.php
@@ -0,0 +1,72 @@
+<?php
+
+use Composer\Composer;
+use Composer\Factory;
+use Composer\Script\Event;
+use Composer\IO\IOInterface;
+use Composer\Package\Link;
+use Composer\Package\Version\VersionParser;
+use Composer\Json\JsonFile;
+
+/**
+ * Class Installer
+ */
+final class Installer
+{
+    private IOInterface $io;
+    private Composer $composer;
+    private JsonFile $composerJson;
+
+    private array $packageRequires;
+    private array $composerDefinitions;
+    
+    public static function install(Event $event)
+    {
+        $installer = new self($event->getComposer(), $event->getIO());
+
+        $result = (int) $installer->io->select('Choose Psr-7 implementation',
+            ['nyholm/psr7', 'laminas/laminas-diactoros'], 'nyholm/psr7'
+        );
+
+        switch ($result)
+        {
+            case 1 : $installer->addPackage('laminas/laminas-diactoros', '^2.6.0');
+                break;
+            default: $installer->addPackage('nyholm/psr7', '^1.4.0');
+        }
+
+        $installer->updateRootPackage();
+        $installer->writeComposerJson();
+    }
+
+    private function __construct(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer; $this->io = $io;
+        $this->packageRequires = $this->composer->getPackage()->getRequires();
+        $this->composerDefinitions = ($this->composerJson = new JsonFile(Factory::getComposerFile()))->read();
+    }
+
+    private function createPackageLink(string $name, string $version): Link
+    {
+        return new Link('__root__', $name, (new VersionParser())->parseConstraints($version), 'requires', $version);
+    }
+
+    private function addPackage(string $name, string $version): void
+    {
+        $this->composerDefinitions['require'][$name] = $version;
+        $this->packageRequires[$name] = $this->createPackageLink($name, $version);
+    }
+
+    private function updateRootPackage(): void
+    {
+        ($package = $this->composer->getPackage())
+            ->setRequires($this->packageRequires);
+
+        $this->composer->setPackage($package);
+    }
+
+    private function writeComposerJson(): void
+    {
+        $this->composerJson->write($this->composerDefinitions);
+    }
+}
