c6a1b60087909d6fe121eeb53e628c31dabe1d55
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-06-14 20:52:30 +0300

    Create Serve.php

---
+++ bin/Commands/Serve.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Console\Commands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class Serve
+ * @package Console\Commands
+ */
+class Serve extends Command
+{
+    public function getName(): string
+    {
+        return 'serve';
+    }
+
+    public function getDescription(): string
+    {
+        return 'Run developer sever';
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        `php -S localhost:8000 -t public//`;
+        return self::SUCCESS;
+    }
+}
