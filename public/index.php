f7739a60fdaf7d6cb5c077165bdd0bb39f20d79d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-06-14 20:28:06 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 error_reporting(E_ALL);
 
+// Delegate static file requests back to the PHP built-in webserver
 if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__)
 {
     return false;
