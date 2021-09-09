541149a84a9c5755058370035900af66ef8a4ded
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-09 23:08:09 +0200

    Update container.php

--- config/container.php
+++ config/container.php
@@ -5,6 +5,5 @@ declare(strict_types = 1);
 use Elie\PHPDI\Config\Config;
 use Elie\PHPDI\Config\ContainerFactory;
 
-
-return (new ContainerFactory)(new Config(require APP_ROOT . '\config\config.php'));
+return (new ContainerFactory)(new Config(require 'config\config.php'));
 
