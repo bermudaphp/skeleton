50a56705e5bce06ec721a3c61e13c5df8cc0251f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-09 22:07:39 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,21 +2,18 @@
 
 declare(strict_types=1);
 
-use Bermuda\Config\Config;
 use Bermuda\App\AppFactory;
 use Bermuda\App\AppInterface;
 use Bermuda\App\Boot\Bootstrapper;
 use Bermuda\App\Boot\BootstrapperInterface;
-use Bermuda\App\Boot\ErrorHandlerBootstrapper;
-use Bermuda\App\Boot\PipelineBootstrapper;
-use Bermuda\App\Boot\RouterBootstrapper;
+use Bermuda\Config\Config;
 use Bermuda\Config\ConfigProvider;
+use Laminas\ConfigAggregator\PhpFileProvider;
+use Monolog\{Handler\StreamHandler, Logger};
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
-use Monolog\{Handler\StreamHandler, Logger};
-use Laminas\ConfigAggregator\PhpFileProvider;
-use App\Handler\HomePageHandler;
 use App\Factory\HomePageHandlerFactory;
+use App\Handler\HomePageHandler;
 
 use function Bermuda\App\{is_console_sapi, path};
 
@@ -30,13 +27,12 @@ return Config::merge(
     new Bermuda\Templater\ConfigProvider(),
     new Bermuda\MiddlewareFactory\ConfigProvider(),
     new Bermuda\ErrorHandler\ConfigProvider(),
-    
-    new PhpFileProvider(APP_ROOT  . '/config/autoload/{{,*.}global,{,*.}local}.php'),
-    new PhpFileProvider(APP_ROOT  . '/config/development.config.php'),
-    
+
+    new PhpFileProvider(APP_ROOT . '/config/autoload/{{,*.}global,{,*.}local}.php'),
+    new PhpFileProvider(APP_ROOT . '/config/development.config.php'),
+
     // App config provider
-    new class extends ConfigProvider
-    {
+    new class extends ConfigProvider {
         /**
          * An associative array that maps a service name to a factory class name, or any callable.
          * Factory classes must be instantiable without arguments, and callable once instantiated (i.e., implement the __invoke() method).
@@ -46,19 +42,17 @@ return Config::merge(
         {
             return [
                 AppInterface::class => AppFactory::class,
-                BootstrapperInterface::class => static function(ContainerInterface $container): Bootstrapper
-                {
+                BootstrapperInterface::class => static function (ContainerInterface $container): Bootstrapper {
                     return Bootstrapper::makeDefault($container);
                 },
-                LoggerInterface::class => static function(ContainerInterface $container)
-                {
+                LoggerInterface::class => static function (ContainerInterface $container) {
                     $is_console = is_console_sapi();
                     return (new Logger($is_console ? 'console' : 'server'))->pushHandler(
                         new StreamHandler(path()->append('logs', $is_console ? 'console.log' : 'server.log'))
                     );
                 },
                 HomePageHandler::class => HomePageHandlerFactory::class,
-           ];
-       }
+            ];
+        }
     },
 );
