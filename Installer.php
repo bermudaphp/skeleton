41b6b2d74804a8cc728a97661d45c54f61378552
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-02 19:57:06 +0200

    Update Installer.php

--- Installer.php
+++ Installer.php
@@ -63,7 +63,7 @@ final class Installer
 
     private function createPackageLink(string $name, string $version): Link
     {
-        return new Link('__root__', $name, (new VersionParser())->parseConstraints($version), 'requires');
+        return new Link('__root__', $name, (new VersionParser())->parseConstraints($version), 'requires', $version);
     }
 
     private function updateRootPackage(): void
