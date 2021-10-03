82ca59b396269c7c68e582bdc0881e2b9d996347
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-03 21:01:51 +0200

    Update .autocomplete.php

--- .autocomplete.php
+++ .autocomplete.php
@@ -1,12 +1,18 @@
 <?php
+
 namespace Autocomplete {
 
+    use Bermuda\App\Config;
+    use Bermuda\Router\Router;
     use Psr\Http\Message\ResponseInterface;
     use Psr\Http\Message\UriInterface;
 
     /**
      * @method ResponseInterface respond(int $code, mixed $content)
      * @method ResponseInterface redirect(string|UriInterface $location, bool $permanent = false)
+     * @method string|ResponseInterface route(string $name, array $params = [], bool $redirect = false)
+     * @property-read Router $router
+     * @property-read Config $config
      */
     class App
     {
