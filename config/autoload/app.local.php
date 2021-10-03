4591a920ef6de31252a4a710a00218c1bf6fe394
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-03 21:11:53 +0200

    Update app.local.php

--- config/autoload/app.local.php
+++ config/autoload/app.local.php
@@ -6,11 +6,5 @@ return [
     'app' => [
         'name' => 'skeleton',
         'version' => '1.0',
-    ],
-
-    'errors' => [
-        'listeners' => static function () {
-            return [LogErrorListener::class];
-        }
     ]
 ];
