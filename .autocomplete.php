b566c27717b6accdff9cd2a72a01adca9f82f39d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-03 10:22:03 +0200

    Create .autocomplete.php

---
+++ .autocomplete.php
@@ -0,0 +1,14 @@
+<?php
+namespace Autocomplete {
+
+    use Psr\Http\Message\ResponseInterface;
+    use Psr\Http\Message\UriInterface;
+
+    /**
+     * @method ResponseInterface respond(int $code, mixed $content)
+     * @method ResponseInterface redirect(string|UriInterface $location, bool $permanent = false)
+     */
+    class App
+    {
+    }
+}
