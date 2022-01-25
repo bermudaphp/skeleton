c0637368ca8521d77f45d2ea663faed37ab85c40
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-01-25 19:10:49 +0200

    Update container.php

--- config/container.php
+++ config/container.php
@@ -3,7 +3,13 @@
 declare(strict_types = 1);
 
 use Elie\PHPDI\Config\Config;
-use Elie\PHPDI\Config\ContainerFactory;
+use Elie\PHPDI\Config\ConfigInterface;
+use Bermuda\App\{Server, Console};
+use Psr\Container\ContainerInterface;
+use function Bermuda\App\is_cli;
 
-return (new ContainerFactory)(new Config(require 'config\config.php'));
+return (static function(ConfigInterface $config, string $containerClass): ContainerInterface {
+        $config->configureContainer($builder = new DI\ContainerBuilder($containerClass));
+        return $builder->build();
+})(new Config(require 'config\config.php'), is_cli() ? Console::class : Server::class);
 
