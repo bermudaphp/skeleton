412b65fcf64730a01b31199e10296c34fed63172
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-01-25 19:12:10 +0200

    Update Command.php

--- bin/Commands/Command.php
+++ bin/Commands/Command.php
@@ -2,11 +2,10 @@
 
 namespace Console\Commands;
 
-use Bermuda\App\Console\Asker\Asker;
 use Bermuda\App\Console\CommandInterface;
-use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
 abstract class Command extends SymfonyCommand implements CommandInterface
 {
