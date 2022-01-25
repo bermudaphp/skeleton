4f41271d3c169ecf1243fdb88589dc5e7f2e882a
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-01-25 13:32:10 +0200

    Update Show.php

--- bin/Commands/Router/Show.php
+++ bin/Commands/Router/Show.php
@@ -2,38 +2,29 @@
 
 namespace Console\Commands\Router;
 
-use function Bermuda\app;
 use Bermuda\App\Boot\RouterBootstrapper;
 use Console\Commands\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * Class Show
- * @package Console\Commands
- */
 final class Show extends Command
 {
+    
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $rows = [];
         $routes = (($app = app())->get(RouterBootstrapper::class))($app);
 
-        foreach($routes->toArray() as $route)
-        {
-            $rows[] = [$route->getName(),
-                $route->getPath(),
-                implode('|', $route->methods())
-            ];
+        foreach($routes->toArray() as $route) {
+            $rows[] = [$route->getName(), $route->getPath(), implode('|', $route->methods())];
         }
 
         ($table = new Table($output))
             ->setHeaders(['Name', 'Path', 'Methods'])
-            ->setRows($rows);
+            ->setRows($rows ?? []);
 
         $table->render();
-
         return self::SUCCESS;
     }
 
