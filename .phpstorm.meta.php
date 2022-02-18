0989a5f2e39f5bcc2aadcac856a6b34df0457dd7
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-18 22:13:53 +0200

    Update .phpstorm.meta.php

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,24 +5,4 @@ namespace PHPSTORM_META {
     override(\Psr\Container\ContainerInterface::get(0), map([
         '' => '@',
     ]));
-
-    override(\Bermuda\ServiceFactory\FactoryInterface::make(0), map([
-        '' => '@',
-    ]));
-
-    override(\Bermuda\app(0), map([
-        '' => '@',
-    ]));
-
-    override(\Bermuda\get(0), map([
-        '' => '@',
-    ]));
-
-    override(\Bermuda\make(0), map([
-        '' => '@',
-    ]));
-
-    override(\Bermuda\service(0), map([
-        '' => '@',
-    ]));
 }
