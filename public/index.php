053b292778aadf0c18260fc60acf42aeab12b26b
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-19 12:45:22 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Bermuda\App\AppInterface;
 use Bermuda\App\Boot\BootstrapperInterface;
-use Psr\Container\ContainerInterface;
+use Bermuda\ErrorHandler\ErrorHandlerInterface;
 
 error_reporting(E_ALL);
 
@@ -17,15 +17,11 @@ chdir(dirname(__DIR__));
 
 require 'vendor\autoload.php';
 
-(static function (ContainerInterface $container): void {
-    $app = $container instanceof AppInterface
-        ? $container : $container->get(AppInterface::class);
-    
+(static function(AppInterface $app, ErrorHandlerInterface $handler, BootstrapperInterface $b): void
+{
     try {
-        $app->get(BootstrapperInterface::class)
-            ->boot($app); $app->run();
-    } catch(Throwable $e) {
-        $app->handleException($e);
+        $b->boot($app)->run();
+    } catch (Throwable $e) {
+        $handler->handleException($e);
     }
-})
-(require 'config\container.php');
+})(... require 'config\container.php');
