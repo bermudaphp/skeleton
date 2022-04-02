5eaab38e5d30a9101d5c5dede96bb66d106ee642
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-04-02 13:00:46 +0200

    Create UseApp.php

---
+++ app/UseApp.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Bermuda\App\AppInterface;
+use Bermuda\App\Boot\BootstrapperInterface;
+
+final class UseApp implements BootstrapperInterface
+{
+    public static AppInterface $app;
+    public function boot(AppInterface $app): AppInterface
+    {
+        if (!function_exists('useApp')) {
+            UseApp::$app = $app;
+            function useApp(): AppInterface {
+                return UseApp::$app;
+            }
+        }
+
+        return $app;
+    }
+}
