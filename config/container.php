62db31053c20b54a47d6cdd0990f535e90a3196f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-01-06 00:34:27 +0200

    Update container.php

--- config/container.php
+++ config/container.php
@@ -2,12 +2,11 @@
 
 declare(strict_types = 1);
 
-use Bermuda\ErrorHandler\ErrorHandlerInterface;
-use Bermuda\App\{AppInterface, Boot\BootstrapperInterface, Server, Console};
+use const Bermuda\App\is_cli;
 use Psr\Container\ContainerInterface;
+use Bermuda\App\{AppInterface, Boot\BootstrapperInterface, Server, Console};
 
 return (static function(array $config, callable $containerFactory = null): array {
-
     if ($containerFactory != null) {
         $container = (static function() use ($containerFactory, $config): ContainerInterface {
             return $containerFactory($config);
