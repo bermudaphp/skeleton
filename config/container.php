812e76681401d13b6bfd8d7f8fe476491d17c67d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-01-12 00:34:56 +0200

    Update container.php

--- config/container.php
+++ config/container.php
@@ -2,18 +2,27 @@
 
 declare(strict_types = 1);
 
-use const Bermuda\App\is_cli;
-use Psr\Container\ContainerInterface;
 use Bermuda\App\{AppInterface, Boot\BootstrapperInterface, Server, Console};
+use Psr\Container\ContainerInterface;
+use const Bermuda\App\is_cli;
 
 return (static function(array $config, callable $containerFactory = null): array {
+
     if ($containerFactory != null) {
         $container = (static function() use ($containerFactory, $config): ContainerInterface {
             return $containerFactory($config);
         })();
     } else {
-        $builder = new DI\ContainerBuilder(\Bermuda\App\is_cli ? Console::class : Server::class);
+        $builder = new DI\ContainerBuilder(is_cli ? Console::class : Server::class);
         (new Elie\PHPDI\Config\Config($config))->configureContainer($builder);
+
+        if (!\Bermuda\Config\Config::$devMode) {
+            $builder->enableCompilation(__DIR__ . '\cache', is_cli ? 'CompiledConsole' : 'CompiledServer',
+                is_cli ? \Bermuda\App\CompiledConsole::class
+                    : \Bermuda\App\CompiledServer::class
+            );
+        }
+
         $container = $builder->build();
     }
     
@@ -22,4 +31,4 @@ return (static function(array $config, callable $containerFactory = null): array
             : $container->get(AppInterface::class),
         $container->get(BootstrapperInterface::class)
     ];
-})(require 'config/config.php');
+})(require 'config\config.php');
