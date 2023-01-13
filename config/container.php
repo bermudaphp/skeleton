a4eed7fee9fa64c51d94d47d846b8342890a9e14
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-01-13 16:36:25 +0200

    Update container.php

--- config/container.php
+++ config/container.php
@@ -2,24 +2,23 @@
 
 declare(strict_types = 1);
 
-use Bermuda\App\{AppInterface, Boot\BootstrapperInterface, Server, Console};
-use Psr\Container\ContainerInterface;
-use const Bermuda\App\is_cli;
-
 return (static function(array $config, callable $containerFactory = null): array {
-
     if ($containerFactory != null) {
-        $container = (static function() use ($containerFactory, $config): ContainerInterface {
+        $container = (static function() use ($containerFactory, $config): Psr\Container\ContainerInterface {
             return $containerFactory($config);
         })();
     } else {
-        $builder = new DI\ContainerBuilder(is_cli ? Console::class : Server::class);
+        $builder = new DI\ContainerBuilder(Bermuda\App\is_cli ?
+            Bermuda\App\Console::class : Bermuda\App\Server::class
+        );
+
         (new Elie\PHPDI\Config\Config($config))->configureContainer($builder);
 
-        if (!\Bermuda\Config\Config::$devMode) {
-            $builder->enableCompilation(__DIR__ . '\cache', is_cli ? 'CompiledConsole' : 'CompiledServer',
-                is_cli ? \Bermuda\App\CompiledConsole::class
-                    : \Bermuda\App\CompiledServer::class
+        if (!Bermuda\Config\Config::$devMode) {
+            $builder->enableCompilation(__DIR__ . '\cache', Bermuda\App\is_cli
+                ? 'CompiledConsole' : 'CompiledServer',
+                Bermuda\App\is_cli ? Bermuda\App\CompiledConsole::class
+                    : Bermuda\App\CompiledServer::class
             );
         }
 
@@ -27,8 +26,8 @@ return (static function(array $config, callable $containerFactory = null): array
     }
     
     return [
-        $container instanceof AppInterface ? $container 
-            : $container->get(AppInterface::class),
-        $container->get(BootstrapperInterface::class)
+        $container instanceof Bermuda\App\AppInterface ? $container
+            : $container->get(Bermuda\App\AppInterface::class),
+        $container->get(Bermuda\App\Boot\BootstrapperInterface::class)
     ];
 })(require 'config\config.php');
