68133f829b62adc63256a9775fd2784009d844d4
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-03-03 17:31:27 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -21,6 +21,7 @@ if (!Config::$devMode && Config::$cacheFile) {
 return Config::merge(
     new Bermuda\App\ConfigProvider,
     new Bermuda\HTTP\ConfigProvider,
+    new Bermuda\Detector\ConfigProvider,
     new Bermuda\PSR7ServerFactory\ConfigProvider,
     new Bermuda\Router\ConfigProvider,
     new Bermuda\Pipeline\ConfigProvider,
