f58aff7db150e7ffffe49328b9a06223f7c13224
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-03-03 17:19:52 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,12 +6,8 @@ use Bermuda\App\AppInterface;
 use Bermuda\App\Boot\BootstrapperInterface;
 
 error_reporting(E_ALL);
-set_error_handler(/**
- * @throws ErrorException
- */ static function(int $errno, string $msg, string $file, int $line): void {
-    if ((error_reporting() & $errno)) {
-        throw new \ErrorException($msg, 0, $errno, $file, $line);
-    }
+set_error_handler(static function(int $errno, string $msg, string $file, int $line): never {
+    throw new \ErrorException($msg, 0, $errno, $file, $line);
 });
 
 try {
@@ -24,8 +20,7 @@ try {
 
     require 'vendor\autoload.php';
 
-    (static function(AppInterface $app, BootstrapperInterface $bootstrapper): void
-    {
+    (static function(AppInterface $app, BootstrapperInterface $bootstrapper): void {
         try {
             $bootstrapper->boot($app)->run();
         } catch (Throwable $e) {
@@ -34,43 +29,6 @@ try {
     })(... require 'config\container.php');
 
 } catch (Throwable $e) {
-
-    $logDir = dirname(__DIR__) . "\logs";
-    $logFile = $logDir . '\\' . ($now = new DateTime())->format('Y-m-d').'.log';
-    $render = static function(string $template, string|array $params): string {
-        is_string($params) ? extract(['msg' => $params]) : extract($params);
-        ob_start();
-        require_once $template;
-        return ob_get_clean();
-    };
-    $renderException = static function() use ($now, $e): string {
-        return sprintf(
-                '[%s][Exception: %s, File: %s, Line: %s, Message: %s, Code: %s]',
-                $now->format('H:i:s p'), $e::class,
-                $e->getFile(), $e->getLine(),
-                $e->getMessage(), $e->getCode()
-            ) . PHP_EOL ;
-    };
-
-    if (!file_exists($logFile) && is_writable($logDir)) {
-        write:
-        file_put_contents($logFile, $renderException(), FILE_APPEND);
-    } elseif (!is_writable($logFile) && chmod($logFile, 0600)) {
-        goto write;
-    } else {
-        if (!chmod($logDir, 0600)) {
-            http_response_code(500);
-            $output = PHP_SAPI === 'cli' ? $renderException()
-                : $render('template/error.phtml','The application ended with an error');
-            exit($output);
-        }
-
-        goto write;
-    }
-
-    http_response_code(500);
-    $output = PHP_SAPI === 'cli' ? $renderException()
-        : $render('template/error.phtml','The application ended with an error');
-    exit($output);
+    (new ErrorHandler())->handle($e);
 }
 
