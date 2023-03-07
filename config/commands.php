b18e184525907f1cf6bdbf0feed5c324242be5b9
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-03-07 17:29:24 +0200

    Update commands.php

--- config/commands.php
+++ config/commands.php
@@ -8,5 +8,4 @@ use Bermuda\App\AppInterface;
 $app->pipe(Console\Commands\Serve::class);
 $app->pipe(Console\Commands\Router\Show::class);
 $app->pipe(Console\Commands\Provider\Push::class);
-$app->pipe(Console\Commands\Make\MakeEndpoint::class);
 
