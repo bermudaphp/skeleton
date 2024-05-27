d926a40a8e193311640cdc4efb4460a2eb567a87
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2024-05-27 17:51:02 +0200

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Bermuda\Router\RouteMap;
+use Bermuda\Router\RouteRecord;
 use Bermuda\App\AppInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -8,6 +9,8 @@ use Psr\Http\Message\ResponseInterface;
  * @var RouteMap $routes
  * @var AppInterface $app
  */
-$routes->get('home', '/', static function() use ($app): ResponseInterface {
-    return $app->respond(200, 'Hello World!');
-});
+$routes->addRoute(
+    RouteRecord::get('home', '/', static function() use ($app): ResponseInterface {
+        return $app->respond(200, 'Hello World!');
+    })
+);
