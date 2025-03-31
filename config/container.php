777a2975ed7dc3a66c6bf7a91a7ecd2fa50570ff
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-03-31 02:44:41 +0200

    Update container.php

--- config/container.php
+++ config/container.php
@@ -2,32 +2,42 @@
 
 declare(strict_types = 1);
 
-return (static function(array $config, callable $containerFactory = null): array {
+return (static function(array $config, ?callable $containerFactory = null): array {
     if ($containerFactory != null) {
         $container = (static function() use ($containerFactory, $config): Psr\Container\ContainerInterface {
             return $containerFactory($config);
         })();
     } else {
-        $builder = new DI\ContainerBuilder(Bermuda\App\is_cli ?
-            Bermuda\App\Console::class : Bermuda\App\Server::class
+        $builder = new DI\ContainerBuilder(PHP_SAPI == 'cli' ?
+            Bermuda\App\ConsoleApp::class : Bermuda\App\ServerApp::class
         );
 
-        (new Elie\PHPDI\Config\Config($config))->configureContainer($builder);
+        new Elie\PHPDI\Config\Config($config)->configureContainer($builder);
 
         if (!Bermuda\Config\Config::$devMode) {
-            $builder->enableCompilation(__DIR__ . '\cache', Bermuda\App\is_cli
+            $builder->enableCompilation(__DIR__ . '\cache', PHP_SAPI == 'cli'
                 ? 'CompiledConsole' : 'CompiledServer',
-                Bermuda\App\is_cli ? Bermuda\App\Compile\CompiledConsole::class
+                PHP_SAPI == 'cli' ? Bermuda\App\Compile\CompiledConsole::class
                     : Bermuda\App\Compile\CompiledServer::class
             );
         }
 
         $container = $builder->build();
     }
+
+    if (isset($config[Bermuda\App\ConfigProvider::CONFIG_KEY_CONTAINERS])
+        && $container instanceof Bermuda\App\ContainerCollectorInterface
+    ) {
+        /**
+         * @var \Psr\Container\ContainerInterface&\Bermuda\App\ContainerCollectorInterface $container
+         */
+        foreach ($config[Bermuda\App\ConfigProvider::CONFIG_KEY_CONTAINERS] as $containerEntry) {
+            $container->addContainer($container->get($containerEntry));
+        }
+    }
     
     return [
-        $container instanceof Bermuda\App\AppInterface ? $container
-            : $container->get(Bermuda\App\AppInterface::class),
-        $container->get(Bermuda\App\Boot\BootstrapperInterface::class)
+        $container->get(Bermuda\App\AppInterface::class),
+        $container->get(Bermuda\App\Boot\Bootable::class)
     ];
 })(require 'config\config.php');
